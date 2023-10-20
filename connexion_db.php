77a491a418cfcb901f62a0997a83f6fc78ca3b88
Klaimand22 <clement.michellod@gmail.com>
2023-10-20 17:23:09 +0200

    Ajout de pleins de chose

---
+++ connexion_db.php
@@ -0,0 +1,30 @@
+<!-- fichier de connexion -->
+<?php
+
+// Connexion à la base de données
+define('SERVEUR_BD', 'localhost');
+define('LOGIN_BD', 'root');
+define('PASS_BD', '');
+define('NOM_BD', 'rallye_video');
+
+
+
+$CONNEXION = mysqli_connect(SERVEUR_BD, LOGIN_BD, PASS_BD);
+
+if (mysqli_connect_errno()) {
+    echo 'Désolé, connexion au serveur ' . SERVEUR_BD . ' impossible, ' . mysqli_connect_error(), "\n";
+    exit();
+  }
+
+  mysqli_select_db($CONNEXION, NOM_BD);
+
+  if (mysqli_connect_errno()) {
+    echo 'Désolé, accès à la base ' . NOM_BD . ' impossible, ' . mysqli_connect_error(), "\n";
+    exit();
+  }
+
+  if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
+    echo 'Error loading character set UTF8: ', mysqli_connect_error(), "\n";
+  }
+
+?>
