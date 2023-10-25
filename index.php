10d7fc4dcf366b3e0b448c830b0ea96f02fb5ea0
Klaimand22 <clement.michellod@gmail.com>
2023-10-25 10:35:18 +0200

    début page index

--- index.php
+++ index.php
@@ -4,10 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rallye Vidéo</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
-    <h1>Rallye Vidéo</h1>
-<h2>Salu klaimand !!!</h2>
+    <?php
+        include("./components/header.html")
+        ?>
+
+
+<main>
+    <div class="left">
+        <img src="./assets/camera_accueil.svg" alt="logo camera">
+
+    </div>
+    <div class="right">
+        <h1>Le rallye vidéo revient !</h1>
+        <!-- décompte -->
+        <p id="countdown"></p>
+    </div>
+</main>
+
 
 </body>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="countdown"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+</script>
 </html>
+
+<?php
+    include("./components/footer.html")
+    ?>
