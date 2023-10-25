6b5a7d8fa22fdcf419a1ea10ba5d064a19e3fb81
Noch22 <noah.fakeadresse@gmail.com>
2023-10-25 09:50:36 +0200

    police + connexion

--- pages/connexion.php
+++ pages/connexion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Connexion</title>
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/connexion.css">
     <link rel="stylesheet" href="../css/header.css">
@@ -13,5 +13,10 @@
     <div class="title">
         <h1>Connexion</h1>
     </div>
+    <form action="" method="post">
+        <input type="email" name="email" placeholder="exemple@mail.fr">
+        <input type="password" name="pass" placeholder="Mot de passe">
+        <button>Connexion</button>
+    </form>
 </body>
 </html>
